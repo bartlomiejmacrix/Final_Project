38538c7a37efd8294f8777579f94a43400aace42
macrix-admin <bartlomiej.szczepanik@macrix.pl>
2024-09-04 15:16:05 +0200

    Add db connection and first migration

--- FinalProject/Program.cs
+++ FinalProject/Program.cs
@@ -1,3 +1,6 @@
+using FinalProject.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +10,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
